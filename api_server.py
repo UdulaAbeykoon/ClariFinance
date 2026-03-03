f6ce00d9da5c02e8d80a13d546388f6418960ecf
Udula <udulaabeykoon@gmail.com>
2026-03-02 20:19:54 -0500

    Migrate chains from langchain to langchain_classic for v1.x compatibility

--- api_server.py
+++ api_server.py
@@ -75,8 +75,8 @@ def init_rag():
         from langchain_openai import ChatOpenAI
         from langchain_huggingface import HuggingFaceEmbeddings
         from langchain_chroma import Chroma
-        from langchain.chains import create_retrieval_chain
-        from langchain.chains.combine_documents import create_stuff_documents_chain
+        from langchain_classic.chains import create_retrieval_chain
+        from langchain_classic.chains.combine_documents import create_stuff_documents_chain
         from langchain_core.prompts import ChatPromptTemplate
 
         embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
